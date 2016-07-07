f8f6a13f4ea62332eae1c5a0dde010da88ace518
scott holland <scotthollandjr@gmail.com>
2016-07-06 18:08:44 -0700

    Add parcel functionality

--- app/src/main/java/com/epicodus/myrestaurants/models/Restaurant.java
+++ app/src/main/java/com/epicodus/myrestaurants/models/Restaurant.java
@@ -1,10 +1,13 @@
 package com.epicodus.myrestaurants;
 
+import org.parceler.Parcel;
+
 import java.util.ArrayList;
 
 /**
  * Created by Guest on 7/5/16.
  */
+@Parcel
 public class Restaurant {
     private String mName;
     private String mPhone;
